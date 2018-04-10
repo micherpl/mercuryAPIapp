f9dbf0059d119b02136692a19b7d6bcd73a89841
MICHALLL <micher.pl@gmail.com>
2018-04-10 17:24:28 +0200

    Initial commit

---
+++ src/main/java/mercuryapiapp/demo/MercuryAPIappApplication.java
@@ -0,0 +1,12 @@
+package mercuryapiapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryAPIappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercuryAPIappApplication.class, args);
+	}
+}
